7da43b67e00bcbc7113f52623d706c965ab29531
Jan Belle <jan.belle@protos.de>
2021-06-17 20:11:47 +0200

    Deprecate resolution of generator module names
    The resolution of generator modules is now handled by eTrice.
    However, we still keep this functionality here for now to retain
    backwards compatibility to older eTrice versions.

--- subprojects/de.protos.etrice.gradle/src/main/java/de/protos/etrice/gradle/GeneratorWorker.java
+++ subprojects/de.protos.etrice.gradle/src/main/java/de/protos/etrice/gradle/GeneratorWorker.java
@@ -4,12 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Optional;
 
+import com.google.inject.Module;
+
 import org.eclipse.etrice.generator.base.GeneratorApplication;
-import org.eclipse.etrice.generator.base.GeneratorException;
-import org.gradle.api.GradleException;
 import org.gradle.api.UncheckedIOException;
 import org.gradle.workers.WorkAction;
 
@@ -18,7 +19,8 @@ import org.gradle.workers.WorkAction;
  * Generator applications are cached and reused.
  */
 public abstract class GeneratorWorker implements WorkAction<GeneratorParameters> {
-	private static final String MODULE_CLASS_NAME_LOCATION = "META-INF/generators/";
+
+	@Deprecated private static final String MODULE_CLASS_NAME_LOCATION = "META-INF/generators/";
 	private static final HashMap<String, GeneratorApplication> CACHE = new HashMap<>();
 	
 	/**
@@ -26,48 +28,44 @@ public abstract class GeneratorWorker implements WorkAction<GeneratorParameters>
 	 */
 	@Override
 	public void execute() {
-		try {
-			String module = getParameters().getModule().get();
-			String[] args = getParameters().getArgs().get();
-			getGeneratorApplication(module).run(args);
-		}
-		catch(GeneratorException e) {
-			throw new GradleException(e.toString(), e);
-		}
+		String name = getParameters().getModule().get();
+		String[] args = getParameters().getArgs().get();
+		CACHE.computeIfAbsent(name, n -> {
+			Module module = createGeneratorModule(n);
+			return GeneratorApplication.create(module);
+		}).run(args);
 	}
 	
 	/**
-	 * Get a generator application by its module name.
+	 * Tries to locate and instantiate the module of a generator by its name.
 	 * 
-	 * @param module the module name of the generator
-	 * @return the generator application instance
-	 */
-	private static GeneratorApplication getGeneratorApplication(String module) {
-		return CACHE.computeIfAbsent(module, m -> {
-			try {
-				String moduleClassName = lookupModuleClassName(m);
-				return GeneratorApplication.create(moduleClassName);
-			}
-			catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-				throw new GradleException(e.toString(), e);
-			}
-		});
-	}
-	
-	/**
-	 * Tries to look up the module class name for a generator.
+	 * @param name the module name of the generator
+	 * @return the module for the generator
+	 * @throws IllegalArgumentException if the generator module could not be located
 	 * 
-	 * @return the module class name for the generator.
+	 * @deprecated The resolution of the generator module is now handled directly by
+	 * {@link GeneratorApplication#create(String)} in eTrice. We still keep this
+	 * functionality here to retain backwards compatibility to older eTrice versions.
 	 */
-	private static String lookupModuleClassName(String module) {
-		try(InputStream in = GeneratorWorker.class.getClassLoader().getResourceAsStream(MODULE_CLASS_NAME_LOCATION + module)) {
-			return Optional.ofNullable(in)
+	@Deprecated
+	private static Module createGeneratorModule(String name) {
+		// Try to read the generator module file to determine the module class name.
+		// If no such file is present, interpret the name itself as module class name to retain backwards compatibility.
+		try(InputStream in = GeneratorWorker.class.getClassLoader().getResourceAsStream(MODULE_CLASS_NAME_LOCATION + name)) {
+			String moduleClassName = Optional.ofNullable(in)
 				.map(is -> new BufferedReader(new InputStreamReader(is)))
 				.flatMap(reader -> reader.lines().findFirst())
-				.orElse(module);
+				.orElse(name);
+			return (Module) Class.forName(moduleClassName).getDeclaredConstructor().newInstance();
+		}
+		catch(ClassNotFoundException e) {
+			throw new IllegalArgumentException("could not find generator with module name " + name, e);
 		}
 		catch(IOException e) {
 			throw new UncheckedIOException(e);
+		}
+		catch(InstantiationException | IllegalAccessException | InvocationTargetException |	NoSuchMethodException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
